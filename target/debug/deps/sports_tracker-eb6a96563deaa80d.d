54becda3b9a3233e7841aa770047e4a5c5c26802
dan-kanu <35091677+dan-kanu@users.noreply.github.com>
2024-08-19 14:02:30 -0500

    first commit

---
+++ target/debug/deps/sports_tracker-eb6a96563deaa80d.d
@@ -0,0 +1,5 @@
+/Users/danielkanu/Desktop/rust/projects/sports_tracker/target/debug/deps/sports_tracker-eb6a96563deaa80d: src/main.rs
+
+/Users/danielkanu/Desktop/rust/projects/sports_tracker/target/debug/deps/sports_tracker-eb6a96563deaa80d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/sports_tracker.d
@@ -0,0 +1,1 @@
+/Users/danielkanu/Desktop/rust/projects/sports_tracker/target/debug/sports_tracker: /Users/danielkanu/Desktop/rust/projects/sports_tracker/src/main.rs
